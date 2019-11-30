338f889dc6f94672122f2f08245bf5a4f436c837
Akhil D Nayak <37106902+akhilnayak0206@users.noreply.github.com>
2019-12-01 00:42:04 +0530

    UI border box

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 style={{flex:'10%'}}>URL SHORTENER</h1>
+        <h1 style={{flex:'10%', fontFamily: "Snell Roundhand, cursive"}}>URL SHORTENER</h1>
       <div className="Display-Center" style={{flex:'90%'}} >
       {!this.state.showUrl ? (
           <div className="Card-Center">
