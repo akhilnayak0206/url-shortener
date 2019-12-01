2bb6dc839e12f20437c0d2ff0d8d04435384db96
Akhil D Nayak <37106902+akhilnayak0206@users.noreply.github.com>
2019-12-01 18:52:18 +0530

    api integartion done

--- src/App.js
+++ src/App.js
@@ -1,62 +1,119 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Card, Input, Button, Icon } from "antd";
+import { Card, Input, Button, Icon, Tooltip } from "antd";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      disabledInput: false,
       urlText: "",
       showUrl: false,
+      urlFinal: "",
+      visibleTooltip: false
     };
   }
 
-  onClickGet = () =>{
-    this.setState({
-      showUrl:true
+  onClickGet = () => {
+    console.log("state", this.state.urlText);
+    const vinay = this.state.urlText;
+    fetch("http://localhost:7000/api/item", {
+      method: "post",
+      body: JSON.stringify({ originalUrl: vinay }),
+      headers: {
+        "Content-Type": "application/json"
+      }
     })
-  }
+      .then(function(response) {
+        console.log("response", response);
+        console.log("responseJson", response);
+        if (response.status === 200) {
+          return response.json();
+        } else {
+          alert("Wrong Url");
+        }
+      })
+      .then(data => {
+        if (JSON.stringify(data)) {
+          this.setState({
+            showUrl: true
+          });
+          console.log("look", JSON.stringify(data));
+          this.setState({ urlFinal: data.shortUrl });
+        }
+      })
+      .catch(err => {
+        console.log("error", err);
+      });
+  };
 
-  onReturnGet = () =>{
+  onReturnGet = () => {
     this.setState({
-      showUrl:false
-    })
-  }
+      showUrl: false,
+      urlText: "",
+      visibleTooltip: false,
+      urlFinal: ""
+    });
+  };
 
   render() {
+    console.log(this.state, "jelklnlssf");
     return (
       <div className="App">
-        <h1 style={{flex:'10%', fontFamily: "Snell Roundhand, cursive"}}>URL SHORTENER</h1>
-      <div className="Display-Center" style={{flex:'90%'}} >
-      {!this.state.showUrl ? (
-          <div className="Card-Center">
-            <Input
-              className="Input-Center"
-              placeholder="Enter URL"
-              onChange={urlText => this.setState({ urlText })}
-              disabled={this.state.disabledInput}
-            />
-            <Button className="Button-Center" type="primary" onClick={()=>this.onClickGet()} >
-              Click For Magic!
-            </Button>
-          </div>
-        ) : null}
-        {this.state.showUrl ? (
-          <Card className="Card-Center" hoverable>
-          <Input
-            className="Input-Center"
-            placeholder="URL will be shown shortly"
-            onChange={urlText => this.setState({ urlText })}
-            disabled
-            addonAfter={<Icon type="copy" />}
-          />
-          <Button className="Button-Center" type="primary" onClick={()=>this.onReturnGet()} >
-              Click To Do Magic Again!
-            </Button>
-        </Card>
-        ) : null}
-      </div>
+        <h1 style={{ flex: "10%", fontFamily: "Snell Roundhand, cursive" }}>
+          URL SHORTENER
+        </h1>
+        <div className="Display-Center" style={{ flex: "90%" }}>
+          {!this.state.showUrl ? (
+            <div className="Card-Center">
+              <Input
+                className="Input-Center"
+                placeholder="Enter URL"
+                onChange={e => this.setState({ urlText: e.target.value })}
+              />
+              <Button
+                className="Button-Center"
+                type="primary"
+                onClick={() => this.onClickGet()}
+              >
+                Click For Magic!
+              </Button>
+            </div>
+          ) : null}
+          {this.state.showUrl ? (
+            <Card className="Card-Center" hoverable>
+              <Input
+                className="Input-Center"
+                placeholder="URL will be shown shortly"
+                value={this.state.urlFinal}
+                disabled
+                addonAfter={
+                  <Tooltip
+                    placement="top"
+                    title={"Copied to Clipboard"}
+                    visible={this.state.visibleTooltip}
+                  >
+                    <CopyToClipboard
+                      text={this.state.urlFinal}
+                      onCopy={() =>
+                        this.setState({ copied: true, visibleTooltip: true })
+                      }
+                    >
+                      <Button icon="copy" />
+                    </CopyToClipboard>
+                  </Tooltip>
+                }
+              />
+              <Button
+                className="Button-Center"
+                type="primary"
+                onClick={() => this.onReturnGet()}
+              >
+                Click To Do Magic Again!
+              </Button>
+            </Card>
+          ) : null}
+        </div>
       </div>
     );
   }
